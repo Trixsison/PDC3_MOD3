4cb42f147e549a26572548773f4aebd5fa5373ff
Trixsison <83437008+Trixsison@users.noreply.github.com>
2022-05-14 14:19:19 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC3_MOD3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void activity1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+        private async void activity2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+        private async void activity3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+        private async void activity4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+        private async void activity5(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity5());
+        }
+        private async void activity6(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity6());
+        }
+    }
+}
---
+++ activity3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC3_MOD3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity3 : ContentPage
+    {
+        public activity3()
+        {
+            InitializeComponent();
+        }
+        private async void backpage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
